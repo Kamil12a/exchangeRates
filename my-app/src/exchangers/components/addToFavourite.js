5bc9444870265a300eeb7bdbbc3ee656916be08e
Kamil-PLewka <kamilplewka12a@wp.pl>
2021-06-20 21:55:46 +0200

    added value to storage

--- my-app/src/exchangers/components/addToFavourite.js
+++ my-app/src/exchangers/components/addToFavourite.js
@@ -1,9 +1,30 @@
+import store from "../../store";
 export const addToFavourite = (event) => {
-  let favouriteExchanges = localStorage.getItem("favourite")
-    ? localStorage.getItem("favourite") + "," + event.target.id
-    : event.target.id;
-  favouriteExchanges = favouriteExchanges.split(",");
-  favouriteExchanges = [...new Set(favouriteExchanges)];
-  favouriteExchanges = favouriteExchanges.join();
-  localStorage.setItem("favourite", favouriteExchanges);
+  if (localStorage.getItem("favourite") === null) {
+    let favouriteExchanges = event.target.id;
+    localStorage.setItem("favourite", favouriteExchanges);
+    let indexOfExchanges = store
+      .getState()
+      .exchanges.list.indexOf(event.target.id);
+    let valuesOfExchanges = store.getState().exchanges.values[indexOfExchanges];
+    localStorage.setItem("values", valuesOfExchanges);
+  } else if (
+    !localStorage.getItem("favourite").split(",").includes(event.target.id)
+  ) {
+    let favouriteExchanges = localStorage.getItem("favourite")
+      ? localStorage.getItem("favourite") + "," + event.target.id
+      : event.target.id;
+    favouriteExchanges = favouriteExchanges.split(",");
+    favouriteExchanges = [...new Set(favouriteExchanges)];
+    favouriteExchanges = favouriteExchanges.join();
+    let indexOfExchanges = store
+      .getState()
+      .exchanges.list.indexOf(event.target.id);
+    let valuesOfExchanges =
+      localStorage.getItem("values") +
+      "," +
+      store.getState().exchanges.values[indexOfExchanges];
+    localStorage.setItem("favourite", favouriteExchanges);
+    localStorage.setItem("values", valuesOfExchanges);
+  }
 };
